36610d5c47def46f595321e6fe9b548efd59e93e
Andres Escobedo <43122995+aescobed@users.noreply.github.com>
2020-07-03 17:23:18 -0700

    Started KMP

---
+++ KMP/KMP Algo/KMP Algo/KMPScanner.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.InteropServices;
+using System.Text;
+
+namespace KMP_Algo
+{
+    class KMPScanner
+    {
+        private string FullText;
+        private string Pattern;
+        private bool CaseSensitive;
+
+        internal KMPScanner(string fullText, string pattern, bool caseSensitive)
+        {
+            this.FullText = fullText;
+            this.Pattern = pattern;
+            this.CaseSensitive = caseSensitive;
+
+            int[] testCharTable = FaultTable(this.Pattern);
+
+            for (int i = 0; i < testCharTable.Length; i++)
+                Console.Write(testCharTable[i]);
+
+
+        }
+
+        // Returns the fault table for where to default when mismatch
+        internal int[] FaultTable(string pattern)
+        {
+
+            int[] res = new int[pattern.Length];
+
+            char[] patChars = pattern.ToCharArray();
+
+            int counter = 0;
+
+            // Find prefixes in pattern
+            for(int i = 1; i < patChars.Length; i++)
+                if(patChars[i] == patChars[counter])
+                {
+                    counter++;
+                    res[i] = counter;
+                }
+                else
+                {
+                    counter = 0;
+                }
+
+            return res;
+        }
+
+    }
+}
---
+++ KMP/KMP Algo/KMP Algo/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace KMP_Algo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            String text = "search this string of strings";
+            String pattern1 = "string";
+            String pattern2 = "repeated";
+            String pattern3 = "aaabbaacdaaabbaac";
+
+            KMPScanner scanner = new KMPScanner(text, pattern3, false);
+        }
+    }
+
+
+
+}
